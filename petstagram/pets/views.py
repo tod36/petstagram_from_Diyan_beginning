7456fe3c3c70c32505000ad9c918f00d524cc4d5
urls <todor36@gmail.com>
2024-09-25 16:11:01 +0300

    feat: added 4 new apps and their views and urls

---
+++ petstagram/pets/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def pet_add_page(request):
+    return render(request, 'pets/pet-add-page.html')
+
+def pet_delete_page(request, username: str, pet_slug: str):
+    return render(request, 'pets/pet-delete-page.html')
+
+def pet_detail_page(request, username: str, pet_slug: str):
+    return render(request, 'pets/pet-details-page.html')
+
+def pet_edit_page(request, username: str, pet_slug: str):
+    return render(request, 'pets/pet-edit-page.html')
